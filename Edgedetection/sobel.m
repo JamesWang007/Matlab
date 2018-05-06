99790d7f46cf27f4011d8fc5e174a589499e8b00
JamesWang007 <wanghaiyang20@gmail.com>
2018-05-06 13:51:36 -0700

    m

--- Edgedetection/sobel.m
+++ Edgedetection/sobel.m
@@ -2,11 +2,13 @@
 %addpath(fullfile(pwd,'images'));
 
 %Sobel Edge Detection 
-I = imread('circuit.tif');
-f_ori = figure,imshow(I);
+I = imread('rocks.jpg');
+I_gray = rgb2gray(I);
+I_gray_double = double(I_gray); 
+f_ori = figure,imshow(I_gray_double);
 message = sprintf('Sobel Edge Detection');
 
-f_c = figure, I_sobel = sobelEdgeDetection(I);
+f_c = figure, I_sobel = sobelEdgeDetection(I_gray_double);
 imshow(I_sobel);
 
 
