77aa0586f2a98e6fc01b836b87c89122812e9a77
JamesWang007 <wanghaiyang20@gmail.com>
2019-01-14 09:03:05 -0800

    /

--- area_detection/pyramid.m
+++ area_detection/pyramid.m
@@ -1,7 +1,14 @@
-function [ output_args ] = pyramid( input_args )
+function [img_py1, img_py2, img_py3, img_py11, img_py22, img_py33] = pyramid( img_py )
 %PYRAMID Summary of this function goes here
 %   Detailed explanation goes here
 
+    img_py1 = impyramid(img_py, 'reduce');
+    img_py2 = impyramid(img_py1, 'reduce');
+    img_py3 = impyramid(img_py2, 'reduce');
 
+    img_py33 = impyramid(img_py3, 'expand');
+    img_py22 = impyramid(img_py33, 'expand');
+    img_py11 = impyramid(img_py22, 'expand');
+    
 end
 
